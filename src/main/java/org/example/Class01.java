4009c6a27b734667cc59092a54da023f8afb2da0
IMGIWON <gwim1130@naver.com>
2023-04-05 10:36:24 +0900

    클래스명 수정

--- src/main/java/org/example/Class01.java
+++ src/main/java/org/example/Class01.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class Class_01 {
+public class Class01 {
     public static void test01() {
         System.out.println("안녕");
     }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Class_01.test01();
+        Class01.test01();
     }
 }
