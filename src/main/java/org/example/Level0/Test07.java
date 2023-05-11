29b075c8977d4861c7ae8cfa140aad4907377b0d
IMGIWON <gwim1130@naver.com>
2023-05-11 15:31:54 +0900

    기초 2 풀이

--- src/main/java/org/example/Level0/Test07.java
+++ src/main/java/org/example/Level0/Test07.java
@@ -10,5 +10,27 @@ package org.example.Level0;
 // 0 ≤ s ≤ my_string의 길이 - overwrite_string의 길이
 public class Test07 {
     public static void main(String[] args) {
+        solution("He11oWor1d","lloWorl",2);
     }
+
+    private static String solution(String my_string, String overwrite_string, int s) {
+        String answer = "";
+        int osCnt = 0;
+        for(int i = 0; i<my_string.length(); i++){
+            if(i>=s && i< overwrite_string.length()+s){
+                answer += overwrite_string.charAt(osCnt);
+                osCnt++;
+            }else{
+                answer += my_string.charAt(i);
+            }
+        }
+        return answer;
+    }
+
+    // 다른사람풀의
+//    public String solution(String my_string, String overwrite_string, int s) {
+//        String before = my_string.substring(0, s);
+//        String after = my_string.substring(s + overwrite_string.length());
+//        return before + overwrite_string + after;
+//    }
 }
